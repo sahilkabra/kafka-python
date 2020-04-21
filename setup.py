e1059dbcb554c27cc63e3b07839e48776f763465
Sahil Kabra <sahil.kabra@lendi.com.au>
2020-04-21 17:14:41 +1000

    pip init

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="kafka-python",
+    version="0.0.1",
+    description="A package that demonstrates kafka producer and consumer",
+    license="MIT",
+    author="Sahil Kabra",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+    ]
+)
