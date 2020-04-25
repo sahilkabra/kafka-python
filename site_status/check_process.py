250a2199c92a3d253f7da68c342649c81c15152e
Sahil Kabra <sahil.kabra@gmail.com>
2020-04-26 09:24:39 +1000

    move connection object to caller

--- site_status/check_process.py
+++ site_status/check_process.py
@@ -14,31 +14,38 @@ def consume(message: str):
 
 def process(response: CheckResponse):
     try:
-        repository = StatusRepository()
-        repository.open_connection()
-        site_id = repository.find_site_id(response.site.name)
+        with StatusRepository.open_connection() as connection:
+            site_id = StatusRepository.find_site_id(connection,
+                                                    response.site.name)
 
-        if site_id is None:
-            site_id = repository.create_site_record(response.site.name,
-                                                    response.site.url)
+            if site_id is None:
+                site_id = StatusRepository.create_site_record(
+                    connection, response.site.name, response.site.url)
 
-        logger.info("found site id {}".format(site_id))
+            assert site_id is not None
+            logger.info("found site id {}".format(site_id))
 
-        response_time: Optional[int] = None
+            response_time: Optional[int] = None
 
-        if response.time_taken is not None:
-            response_time = round(response.time_taken * 1000)
+            if response.time_taken is not None:
+                response_time = round(response.time_taken * 1000)
 
-        entity = CheckEntity(id=None,
-                             site_id=site_id,
-                             check_time=response.date,
-                             http_status_code=response.status_code,
-                             http_status_reason=response.status_message,
-                             response_time=response_time)
+            entity = CheckEntity(id=None,
+                                 site_id=site_id,
+                                 check_time=response.date,
+                                 http_status_code=response.status_code,
+                                 http_status_reason=response.status_message,
+                                 response_time=response_time)
 
-        site_check_record = repository.create_site_check_record(entity)
-        repository.commit()
+            site_check_record = StatusRepository.create_site_check_record(
+                connection, entity)
 
-        logger.info("created site check record {}".format(site_check_record))
+            connection.commit()
+
+            assert site_check_record is not None
+            logger.info("created site check record {} for {}".format(
+                site_check_record.id, response.site.name))
+
     finally:
-        repository.close()
+        if connection.closed == 0:
+            connection.close()
--- site_status/repository.py
+++ site_status/repository.py
@@ -19,36 +19,33 @@ FIND_SITE_CHECK_QUERY = """select id, site_id, time, http_status_code, http_reas
 
 
 class StatusRepository:
-    def open_connection(self):
-        self.connection = psycopg2.connect(
-            host=database_config["host"],
-            port=database_config["port"],
-            sslmode="require",
-            database=database_config["database"],
-            user=database_config["user"],
-            password=database_config["password"])
+    @staticmethod
+    def open_connection():
+        return psycopg2.connect(host=database_config["host"],
+                                port=database_config["port"],
+                                sslmode="require",
+                                database=database_config["database"],
+                                user=database_config["user"],
+                                password=database_config["password"])
 
-    def close(self):
-        self.connection.close()
-
-    def commit(self):
-        self.connection.commit()
-
-    def find_site_id(self, name: str) -> Optional[int]:
-        with self.connection.cursor() as cursor:
+    @staticmethod
+    def find_site_id(connection, name: str) -> Optional[int]:
+        with connection.cursor() as cursor:
             cursor.execute(FIND_SITE_ID_QUERY, (name, ))
             row = cursor.fetchone()
             return None if row is None else row[0]
 
-    def create_site_record(self, name: str, url: str) -> Optional[int]:
-        with self.connection.cursor() as cursor:
+    @staticmethod
+    def create_site_record(connection, name: str, url: str) -> Optional[int]:
+        with connection.cursor() as cursor:
             cursor.execute(INSERT_SITE_QUERY, (name, url))
 
-        return self.find_site_id(name)
+        return StatusRepository.find_site_id(connection, name)
 
-    def create_site_check_record(self,
+    @staticmethod
+    def create_site_check_record(connection,
                                  entity: CheckEntity) -> Optional[CheckEntity]:
-        with self.connection.cursor() as cursor:
+        with connection.cursor() as cursor:
             cursor.execute(
                 INSERT_SITE_CHECK_QUERY, {
                     "site_id": entity.site_id,
@@ -58,10 +55,13 @@ class StatusRepository:
                     "response_time": entity.response_time,
                 })
 
-        return self.find_latest_site_check(entity.site_id)
+        return StatusRepository.find_latest_site_check(connection,
+                                                       entity.site_id)
 
-    def find_latest_site_check(self, site_id: int) -> Optional[CheckEntity]:
-        with self.connection.cursor() as cursor:
+    @staticmethod
+    def find_latest_site_check(connection,
+                               site_id: int) -> Optional[CheckEntity]:
+        with connection.cursor() as cursor:
             cursor.execute(FIND_SITE_CHECK_QUERY, (site_id, ))
             row = cursor.fetchone()
 
