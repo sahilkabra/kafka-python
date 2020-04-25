631f4703027d8c31664d932a87b928eeb9f5124f
Sahil Kabra <sahil.kabra@gmail.com>
2020-04-25 15:16:12 +1000

    db config

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-kafkaConfig = {
+kafka_config = {
     "uri": "kafka-kafkapython-sample-sahil-97de.aivencloud.com:16096",
     "ca_path": ".env/ca.pem",
     "cert_path": ".env/service.cert",
@@ -6,7 +6,7 @@ kafkaConfig = {
     "topic": "sample-topic",
 }
 
-sitesConfig = [{
+sites_config = [{
     "url": "https://google.com.au",
     "regex": "Google"
 }, {
@@ -16,3 +16,11 @@ sitesConfig = [{
     "url": "http://somenonexistentsite",
     "regex": ""
 }]
+
+database_config = {
+    "host": "pg-kafkapython-sample-sahil-97de.aivencloud.com",
+    "port": 16094,
+    "user": "",
+    "password": "",
+    "cert_path": ".env/postgresql.pem"
+}
